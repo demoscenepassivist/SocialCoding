67a31e293d5293a1c367dab44ade23513f7028ed
DemoscenePassivist <demoscenepassivist@googlemail.com>
2012-05-23 22:24:20 +0200

    Some cleanup maintenance. Deleted a couple of legacy batchfiles.

--- build_demos_jogamp/Jogamp_CaptureConversion_XVID-HQ.bat
+++
@@ -1,6 +0,0 @@
-echo on
-cd .
-set PATH=.;.\tools\;.\capture\;
-cd .\capture\
-mencoder.exe "mf://*.png" -mf fps=60 -o "..\Jogamp_Capture.avi" -ovc xvid -xvidencopts fixed_quant=4 bitrate=25000000 -vf harddup
-
--- build_demos_jogamp/Jogamp_Standalone_JOGL_GL3_Raymarcher_Mandelbulb.bat
+++
@@ -1,7 +0,0 @@
-echo on
-cd .
-set PATH=.;.\runtime\bin\;.\native\;
-set JAVA_HOME=.\runtime\;
-.\runtime\bin\java.exe -version
-.\runtime\bin\java.exe -client -Dsun.java2d.noddraw=true -Dsun.awt.noerasebackground=true -XX:+AggressiveOpts -Xcomp -Xnoclassgc -XX:+UseConcMarkSweepGC -Xms64m -Xmx512m -jar jogamp.jar -RESOLUTION=AUTO -ROUTINE=jogamp.routine.jogl.programmablepipeline.GL3_Raymarcher_Mandelbulb -FRAMERATE=AUTO -FULLSCREEN=FALSE -VSYNC=TRUE -MULTISAMPLING=FALSE -SAMPLEBUFFERS=4 -ANISOTROPICFILTERING=FALSE -ANISOTROPYLEVEL=16.0 -FRAMESKIP=FALSE -FRAMECAPTURE=FALSE -WINDOWTOOLKIT=AWT
-SET /P consolewait="PRESS RETURN TO KILL CONSOLE ..."
